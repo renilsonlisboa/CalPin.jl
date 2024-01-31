b1059280d88c333f238bc08fb86a5cfc2d9ac96a
Renilson Lisboa Júnior <renilsonlisboajunior@gmail.com>
2024-01-31 09:57:48 -0300

    modified:   src/CalPin.jl

--- src/CalPin.jl
+++ src/CalPin.jl
@@ -1,8 +1,8 @@
 module CalPin
 
 # Inclui os módulos auxiliares no projeto
-include(joinpath(@__DIR__, "src/Altura.jl"))
-include(joinpath(@__DIR__, "src/Volume.jl"))
+include(joinpath(@__DIR__, "Altura.jl"))
+include(joinpath(@__DIR__, "Volume.jl"))
 
 import QML: QString, @qmlfunction, loadqml, exec
 
@@ -38,7 +38,7 @@ export RunApp
         current_directory = dirname(@__FILE__)
 
         # Carrega o arquivo .qml presente no diretório do pacote
-        loadqml(joinpath(current_directory, "src/qml", "main.qml"))
+        loadqml(joinpath(current_directory, "qml", "main.qml"))
 
         # Inicializa o arquivo .qml
         exec()
